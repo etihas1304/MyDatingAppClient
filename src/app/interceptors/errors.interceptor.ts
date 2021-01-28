3ce6b5eeb6689dd239ff27b38ea4d6f50c56da47
eti has <etihas1304@gmail.com>
2021-01-28 20:14:45 +0200

    section 14

--- src/app/interceptors/errors.interceptor.ts
+++ src/app/interceptors/errors.interceptor.ts
@@ -30,8 +30,11 @@ export class ErrorsInterceptor implements HttpInterceptor {
                 }
                 throw modelStateErrors.flat();
                 
-              }else{
+              }else if (typeof(error.error) === 'object') {
                 this.toastr.error(error.statusText,error.status);
+              }
+              else {
+                this.toastr.error(error.error, error.status);
               }
               
               break;
